ccf6b25a50fc5b38f8e79d40c4dac3b135dd0e86
JacovkaMana <63916620+JacovkaMana@users.noreply.github.com>
2022-11-06 15:03:01 +0300

    Django app

--- inno/settings.py
+++ inno/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig',
     'applications.apps.ApplicationsConfig',
     'rest_framework',
 
